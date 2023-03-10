51a47eda427c3cbc4afe13316b9b6482c72ebe04
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-03-10 12:25:04 +0100

    Adds splash screen

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,17 +3,3 @@ require('laravel-mix-jigsaw');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
-
-mix.js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css/main.css')
-    .jigsaw({
-        watch: ['config.php', 'source/**/*.md', 'source/**/*.php', 'source/**/*.scss'],
-    })
-    .options({
-        processCssUrls: false,
-        postCss: [
-            require('tailwindcss'),
-        ],
-    })
-    .sourceMaps()
-    .version();
